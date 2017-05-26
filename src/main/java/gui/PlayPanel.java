08737535f2a533f1557dd25fff08acff192f424e
Joao Chaves <joaovitor1906@hotmail.com>
2017-05-26 16:55:56 +0100

    cards template added to play panel

--- src/main/java/gui/PlayPanel.java
+++ src/main/java/gui/PlayPanel.java
@@ -8,7 +8,17 @@ import java.awt.*;
  */
 public class PlayPanel extends JFrame {
 
+    public static JTextArea createVoidCard(){
+        JTextArea card = new JTextArea("empty text card");
+        card.setLineWrap(true);
+        card.setWrapStyleWord(true);
+        card.setBackground(Color.white);
+        card.setForeground(Color.black);
+        card.setFont(new Font("Georgia", Font.BOLD, 14));
+        card.setEditable(false);
 
+        return card;
+    }
 
     public static void main(String[] args){
         JFrame frame = new JFrame("Grid Layout");
@@ -16,47 +26,44 @@ public class PlayPanel extends JFrame {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800,550);
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(4,7,7,5));
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 10, 20, 10));
+        panel.setLayout(new GridLayout(4,7,20,15));
         JLabel card1 = new JLabel("BlackCard",SwingConstants.CENTER);
         card1.setForeground(Color.white);
-        JLabel card2 = new JLabel("Player 1 card 1",SwingConstants.CENTER);
-        JLabel card3 = new JLabel("Player 2 card 1",SwingConstants.CENTER);
-        JLabel card4 = new JLabel("Player 3 card 1",SwingConstants.CENTER);
-        JLabel card5 = new JLabel("Player 4 card 1",SwingConstants.CENTER);
-        JLabel card6 = new JLabel("Player 5 card 1",SwingConstants.CENTER);
+
+        JTextArea card2 = createVoidCard();
+        JTextArea card3 = createVoidCard();
+        JTextArea card4 = createVoidCard();
+        JTextArea card5 = createVoidCard();
+        JTextArea card6 = createVoidCard();
         JLabel card7 = new JLabel("");
 
         JLabel card8 = new JLabel("");
-        JLabel card9 = new JLabel("Player 1 card 2",SwingConstants.CENTER);
-        JLabel card10 = new JLabel("Player 2 card 2",SwingConstants.CENTER);
-        JLabel card11 = new JLabel("Player 3 card 2",SwingConstants.CENTER);
-        JLabel card12 = new JLabel("Player 4 card 2",SwingConstants.CENTER);
-        JLabel card13 = new JLabel("Player 5 card 2",SwingConstants.CENTER);
+        JTextArea card9 = createVoidCard();
+        JTextArea card10 = createVoidCard();
+        JTextArea card11 = createVoidCard();
+        JTextArea card12 = createVoidCard();
+        JTextArea card13 = createVoidCard();
         JLabel card14 = new JLabel("");
 
-        JLabel card15 = new JLabel("",SwingConstants.CENTER);
-        JLabel card16 = new JLabel("",SwingConstants.CENTER);
-        JLabel card17 = new JLabel("",SwingConstants.CENTER);
-        JLabel card18 = new JLabel("",SwingConstants.CENTER);
-        JLabel card19 = new JLabel("",SwingConstants.CENTER);
-        JLabel card20 = new JLabel("",SwingConstants.CENTER);
+        JLabel card15 = new JLabel("");
+        JLabel card16 = new JLabel("");
+        JLabel card17 = new JLabel("");
+        JLabel card18 = new JLabel("");
+        JLabel card19 = new JLabel("");
+        JLabel card20 = new JLabel("");
+
         JLabel card21 = new JLabel("");
+        JLabel card22 = new JLabel("");
+        JTextArea card23 = createVoidCard();
+        JTextArea card24 = createVoidCard();
+        JTextArea card25 = createVoidCard();
+        JTextArea card26 = createVoidCard();
+        JTextArea card27 = createVoidCard();
 
-        JPanel confirm = new JPanel(new GridLayout(3, 1,0, 10));
-        JButton confirmBtn = new JButton("Confirm");
-        confirm.add(new JLabel(""));
-        confirm.add(confirmBtn);
-        confirm.add(new JLabel(""));
+        JLabel card28 = new JLabel("");
 
-
-        JLabel card22 = new JLabel("Card1",SwingConstants.CENTER);
-        JLabel card23 = new JLabel("Card2",SwingConstants.CENTER);
-        JLabel card24 = new JLabel("Card3",SwingConstants.CENTER);
-        JLabel card25 = new JLabel("Card4",SwingConstants.CENTER);
-        JLabel card26 = new JLabel("Card5",SwingConstants.CENTER);
-        JLabel card27 = new JLabel("");
-
-
+        panel.setBackground(Color.black);
 
         panel.add(card1);
         panel.add(card2);
@@ -82,19 +89,17 @@ public class PlayPanel extends JFrame {
         panel.add(card20);
         panel.add(card21);
 
-        confirm.setBackground(Color.black);
-        panel.add(confirm);
         panel.add(card22);
         panel.add(card23);
         panel.add(card24);
         panel.add(card25);
         panel.add(card26);
         panel.add(card27);
+        panel.add(card28);
 
-        panel.setBackground(Color.black);
         panel.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
         frame.add(panel);
-
+        frame.setVisible(true);
     }
 
 }
